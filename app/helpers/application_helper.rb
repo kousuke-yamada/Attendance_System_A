e896c7adb6eba9f2db0450f3982a7c26bf732527
kousuke yamada <offmailbox1225@gmail.com>
2022-07-11 14:57:17 +0000

    Finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # ページごとにタイトルを返す
+  def full_title(page_name = "")
+    base_title = "AttendanceApp"
+    if page_name.empty?
+      base_title
+    else
+      page_name + " | " + full_title
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get 'static_pages/top'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#top'
 end
