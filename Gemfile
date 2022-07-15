e81c732e89a8df1d0618668764cf08c06f110afe
kousuke yamada <offmailbox1225@gmail.com>
2022-07-15 17:28:28 +0000

    Finish japanization-and-time-format

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails',        '~> 5.1.6'
+gem 'rails-i18n'
 gem 'bcrypt'
 gem 'faker'
 gem 'bootstrap-sass'
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# ページネーションラベルの設定
+WillPaginate::ViewHelpers.pagination_options[:previous_label] = '前ページ'
+WillPaginate::ViewHelpers.pagination_options[:next_label] = '次ページ'
